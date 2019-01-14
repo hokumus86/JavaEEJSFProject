1eb2767fd596dd5d437320a24a3a28a9ff12252c
hokumus86 <vektorel@lab9-ogr>
2019-01-14 20:02:05 +0200

    jsf sdfjsdklj

---
+++ src/beans/LoginBean.java
@@ -0,0 +1,44 @@
+package beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+
+@ManagedBean
+@SessionScoped
+public class LoginBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6955508471291131930L;
+	
+	
+	private String userName;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String giris() {
+		System.out.println("Username :" + userName +" þifre :" + password);
+		return "";
+	}
+
+}
